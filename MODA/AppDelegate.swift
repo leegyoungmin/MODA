ce4d031612e97b3f29e5626953e2a0fd12329f77
미니 <cow970814@naver.com>
2023-05-22 14:58:39 +0900

    chore : SwiftLint 규칙 문서 변경

--- MODA/AppDelegate.swift
+++ MODA/AppDelegate.swift
@@ -3,14 +3,19 @@
 //  MODA
 //
 //  Copyright (c) 2023 Minii All rights reserved.
-        
 
 import UIKit
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-  func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-    return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+  func application(
+    _ application: UIApplication,
+    configurationForConnecting connectingSceneSession: UISceneSession,
+    options: UIScene.ConnectionOptions
+  ) -> UISceneConfiguration {
+    return UISceneConfiguration(
+      name: "Default Configuration",
+      sessionRole: connectingSceneSession.role
+    )
   }
 }
-
--- MODA/SceneDelegate.swift
+++ MODA/SceneDelegate.swift
@@ -3,14 +3,17 @@
 //  MODA
 //
 //  Copyright (c) 2023 Minii All rights reserved.
-        
 
 import UIKit
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
   var window: UIWindow?
 
-  func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+  func scene(
+    _ scene: UIScene,
+    willConnectTo session: UISceneSession,
+    options connectionOptions: UIScene.ConnectionOptions
+  ) {
     guard let windowScene = (scene as? UIWindowScene) else { return }
     window = UIWindow(windowScene: windowScene)
     window?.rootViewController = ViewController()
