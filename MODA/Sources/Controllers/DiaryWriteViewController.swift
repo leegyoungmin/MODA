f5600c57b302968287d262dc97baaabecfe8de51
미니 <cow970814@naver.com>
2023-05-23 23:48:42 +0900

    feat : Write View Controller UI 변경

--- MODA/Sources/Controllers/DiaryWriteViewController.swift
+++ MODA/Sources/Controllers/DiaryWriteViewController.swift
@@ -10,6 +10,7 @@ import SnapKit
 final class DiaryWriteViewController: UIViewController {
     private let conditionTitleLabel: UILabel = {
         let label = UILabel()
+        label.font = .systemFont(ofSize: 16, weight: .semibold)
         label.translatesAutoresizingMaskIntoConstraints = false
         label.text = "오늘은 컨디션이 어떤가요?"
         return label
@@ -17,6 +18,7 @@ final class DiaryWriteViewController: UIViewController {
     
     private let contentTitleLabel: UILabel = {
         let label = UILabel()
+        label.font = .systemFont(ofSize: 16, weight: .semibold)
         label.text = "오늘을 기억할 수 있는 말을 적어보세요."
         return label
     }()
@@ -29,9 +31,8 @@ final class DiaryWriteViewController: UIViewController {
         let stackView = UIStackView()
         stackView.axis = .horizontal
         stackView.alignment = .fill
-        stackView.spacing = 12
+        stackView.spacing = 24
         stackView.distribution = .fillEqually
-        stackView.layoutMargins = UIEdgeInsets(top: 12, left: 12, bottom: 12, right: 12)
         stackView.isLayoutMarginsRelativeArrangement = true
         return stackView
     }()
@@ -39,8 +40,21 @@ final class DiaryWriteViewController: UIViewController {
     private let contentTextView: UITextView = {
         let textView = UITextView()
         textView.layer.backgroundColor = UIColor(named: "SecondaryColor")?.cgColor
+        textView.font = .systemFont(ofSize: 20)
+        textView.textContainerInset = UIEdgeInsets(top: 12, left: 12, bottom: 12, right: 12)
         textView.layer.cornerRadius = 14
         return textView
+    }()
+    
+    private let saveButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("저장하기", for: .normal)
+        button.titleLabel?.font = .systemFont(ofSize: 22, weight: .bold)
+        button.setTitleColor(.white, for: .normal)
+        button.layer.backgroundColor = UIColor(named: "AccentColor")?.cgColor
+        button.layer.cornerRadius = 12
+        button.contentEdgeInsets = UIEdgeInsets(top: 12, left: 0, bottom: 12, right: 0)
+        return button
     }()
     
     override func viewDidLoad() {
@@ -53,33 +67,44 @@ final class DiaryWriteViewController: UIViewController {
 
 private extension DiaryWriteViewController {
     func configureUI() {
+        configureNavigationBar()
         configureHierarchy()
         makeConstraints()
     }
     
     func configureHierarchy() {
-        [goodConditionButton, normalConditionButton, badConditionButton]
+        [badConditionButton, normalConditionButton, goodConditionButton]
             .forEach(conditionStackView.addArrangedSubview)
-        [conditionTitleLabel, contentTitleLabel, contentTextView, conditionStackView]
+        [conditionTitleLabel, contentTitleLabel, contentTextView, conditionStackView, saveButton]
             .forEach(view.addSubview)
+    }
+    
+    func configureNavigationBar() {
+        let dismissAction = UIAction { [weak self] _ in
+            self?.dismiss(animated: true)
+        }
+        let dismissButton = UIBarButtonItem(
+            image: UIImage(systemName: "xmark.circle.fill"),
+            primaryAction: dismissAction
+        )
+        navigationItem.leftBarButtonItem = dismissButton
     }
     
     func makeConstraints() {
         conditionTitleLabel.snp.makeConstraints {
-            $0.top.equalToSuperview()
+            $0.top.equalTo(view.safeAreaLayoutGuide.snp.top).offset(12)
             $0.leading.equalToSuperview().offset(22)
             $0.trailing.equalToSuperview().offset(-22)
         }
         
         conditionStackView.snp.makeConstraints {
             $0.top.equalTo(conditionTitleLabel.snp.bottom).offset(12)
-            $0.centerX.equalToSuperview()
-            $0.width.equalTo(view.snp.width).multipliedBy(0.9)
-            $0.height.equalTo(view.snp.width).multipliedBy(0.3)
+            $0.leading.equalToSuperview().offset(22)
+            $0.trailing.equalToSuperview().offset(-22)
         }
         
         contentTitleLabel.snp.makeConstraints {
-            $0.top.equalTo(conditionStackView.snp.bottom).offset(12)
+            $0.top.equalTo(conditionStackView.snp.bottom).offset(30)
             $0.leading.equalToSuperview().offset(22)
             $0.trailing.equalToSuperview().offset(-22)
         }
@@ -88,7 +113,14 @@ private extension DiaryWriteViewController {
             $0.top.equalTo(contentTitleLabel.snp.bottom).offset(12)
             $0.leading.equalToSuperview().offset(22)
             $0.trailing.equalToSuperview().offset(-22)
-            $0.height.equalTo(view.snp.height).multipliedBy(0.6)
+            $0.height.equalTo(view.snp.height).multipliedBy(0.5)
+        }
+        
+        saveButton.snp.makeConstraints {
+            $0.top.equalTo(contentTextView.snp.bottom).offset(28)
+            $0.leading.equalTo(view.snp.leading).offset(22)
+            $0.trailing.equalTo(view.snp.trailing).offset(-22)
+            $0.bottom.equalTo(view.safeAreaLayoutGuide.snp.bottom).offset(-12)
         }
     }
 }
