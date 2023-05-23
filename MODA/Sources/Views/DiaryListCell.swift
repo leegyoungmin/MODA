dccd586e85cd20a4072024bb1f57ea31eec54318
미니 <cow970814@naver.com>
2023-05-23 21:08:42 +0900

    feat : Diary List Cell Content 설정

--- MODA/Sources/Views/DiaryListCell.swift
+++ MODA/Sources/Views/DiaryListCell.swift
@@ -14,7 +14,7 @@ final class DiaryListCell: UITableViewCell {
     private let createdDateLabel: UILabel = {
         let label = UILabel()
         label.translatesAutoresizingMaskIntoConstraints = false
-        label.font = .preferredFont(forTextStyle: .title1)
+        label.font = .preferredFont(forTextStyle: .body)
         return label
     }()
     
@@ -26,9 +26,25 @@ final class DiaryListCell: UITableViewCell {
         return label
     }()
     
+    private let dividerView: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.backgroundColor = .secondaryLabel
+        return view
+    }()
+    
+    private let contentLabel: UILabel = {
+        let label = UILabel()
+        label.numberOfLines = 5
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
         
+        contentView.backgroundColor = UIColor(named: "AccentColor")
+        contentView.layer.cornerRadius = 12
         configureUI()
     }
     
@@ -41,6 +57,7 @@ final class DiaryListCell: UITableViewCell {
     func setUpDatas(to diary: Diary) {
         createdDateLabel.text = diary.meta.createdDate.description
         conditionLabel.text = diary.condition.rawValue.description
+        contentLabel.text = diary.content
     }
 }
 
@@ -51,18 +68,35 @@ private extension DiaryListCell {
     }
     
     func configureHierarchy() {
-        [createdDateLabel, conditionLabel].forEach(contentView.addSubview)
+        [createdDateLabel, conditionLabel, dividerView, contentLabel]
+            .forEach(contentView.addSubview)
     }
     
     func makeConstraints() {
         createdDateLabel.snp.makeConstraints {
-            $0.top.leading.equalToSuperview().offset(12)
+            $0.top.leading.equalToSuperview()
+            $0.height.lessThanOrEqualTo(50)
         }
         
         conditionLabel.snp.makeConstraints {
             $0.top.equalTo(createdDateLabel.snp.bottom)
             $0.leading.equalToSuperview().offset(12)
             $0.bottom.equalToSuperview().offset(-12)
+        }
+        
+        dividerView.snp.makeConstraints {
+            $0.top.equalTo(createdDateLabel.snp.top).offset(12)
+            $0.leading.equalTo(createdDateLabel.snp.trailing).offset(12)
+            $0.bottom.equalTo(conditionLabel.snp.bottom)
+            $0.width.equalTo(1)
+        }
+        
+        contentLabel.snp.makeConstraints {
+            $0.top.equalTo(dividerView.snp.top)
+            $0.bottom.equalTo(dividerView.snp.bottom)
+            $0.leading.equalTo(dividerView.snp.trailing).offset(12)
+            $0.trailing.equalToSuperview()
+            $0.width.equalToSuperview().multipliedBy(0.8)
         }
     }
 }
