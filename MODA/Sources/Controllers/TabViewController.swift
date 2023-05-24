62f22cc45e5ede29b2b926e158182701ede89e79
미니 <cow970814@naver.com>
2023-05-24 18:48:49 +0900

    refactor :  Tab View Controller Router 타입 생성

--- MODA/Sources/Controllers/TabViewController.swift
+++ MODA/Sources/Controllers/TabViewController.swift
@@ -7,22 +7,62 @@
 import UIKit
 
 final class TabViewController: UITabBarController {
-    
-    private let listViewController = DiaryListViewController()
+    private let routers = Router.allCases
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let tabBarItem = UITabBarItem(
-            title: "홈",
-            image: UIImage(systemName: "list.bullet"),
-            tag: 0
-        )
+        let controllers = routers.map {
+            let tabItem = configureTabBarItem(with: $0)
+            let controller = $0.instance
+            controller.tabBarItem = tabItem
+            return UINavigationController(rootViewController: controller)
+        }
         
-        listViewController.tabBarItem = tabBarItem
-        
-        let controllers = [listViewController]
-            .map { UINavigationController(rootViewController: $0) }
         setViewControllers(controllers, animated: true)
+    }
+    
+    private func configureTabBarItem(with router: Router) -> UITabBarItem {
+        return UITabBarItem(
+            title: router.title,
+            image: UIImage(systemName: router.imageName),
+            tag: router.rawValue
+        )
+    }
+}
+
+private extension TabViewController {
+    enum Router: Int, CaseIterable {
+        case home
+        case save
+        
+        var title: String {
+            switch self {
+            case .home:
+                return "홈"
+            case .save:
+                return "저장일기"
+            }
+        }
+        
+        var imageName: String {
+            switch self {
+            case .home:
+                return "list.bullet"
+                
+            case .save:
+                return "star"
+            }
+        }
+        
+        var instance: UIViewController {
+            switch self {
+            case .home:
+                return DiaryListViewController()
+                
+            case .save:
+                return SavedDiaryViewController()
+            }
+        }
     }
 }
