57a37e5f53d751d582241fca694ac1c1c226d07f
미니 <cow970814@naver.com>
2023-05-24 23:32:27 +0900

    feat : 설정 화면 TableView 생성

--- MODA/Sources/Controllers/SettingViewController.swift
+++ MODA/Sources/Controllers/SettingViewController.swift
@@ -7,9 +7,33 @@
 import UIKit
 
 final class SettingViewController: UIViewController {
+    
+    private let settingTableView: UITableView = {
+        let tableView = UITableView(frame: .zero, style: .insetGrouped)
+        tableView.contentInset = UIEdgeInsets(top: 16, left: 0, bottom: 16, right: 0)
+        return tableView
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.backgroundColor = .red
+        configureUI()
+    }
+}
+
+private extension SettingViewController {
+    func configureUI() {
+        configureHierarchy()
+        makeConstraints()
+    }
+    
+    func configureHierarchy() {
+        [settingTableView].forEach(view.addSubview)
+    }
+    
+    func makeConstraints() {
+        settingTableView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
     }
 }
