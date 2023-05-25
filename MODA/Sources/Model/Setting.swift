66fefd9d45c11c5b4693ef064b5e2017b6c1d177
미니 <cow970814@naver.com>
2023-05-25 15:27:14 +0900

    feat : 설정 타입 구현 및 기본 값 구현

---
+++ MODA/Sources/Model/Setting.swift
@@ -0,0 +1,38 @@
+//
+//  Setting.swift
+//  MODA
+//
+//  Copyright (c) 2023 Minii All rights reserved.
+
+struct SettingSection {
+    let title: String
+    let options: [SettingOption]
+}
+
+struct SettingOption {
+    let title: String
+    let content: String
+    
+    let handler: () -> Void
+}
+
+extension SettingSection {
+    static let defaultSettings: [SettingSection] = [
+        .init(title: "알림 설정", options: [
+            .init(title: "알림 시간", content: "09:00") {
+                print("did tap Present Alert Button")
+            }
+        ]),
+        .init(title: "고객 센터", options: [
+            .init(title: "의견보내기", content: "") {
+                
+            },
+            .init(title: "별점남기기", content: "") {
+                
+            },
+            .init(title: "개인정보처리방침", content: "") {
+                
+            }
+        ])
+    ]
+}
