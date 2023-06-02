5ba74256c0c795b1a327e9b5b7f6aec9fea5d785
미니 <cow970814@naver.com>
2023-06-02 23:42:17 +0900

    feat : Diary Write Use Case 파일 추가

---
+++ MODA/Sources/Domain/UseCase/Diaries/DefaultDiaryWriteUseCase.swift
@@ -0,0 +1,11 @@
+//
+//  DefaultDiaryWriteUseCase.swift
+//  MODA
+//
+//  Copyright (c) 2023 Minii All rights reserved.
+
+import RxSwift
+
+final class DefaultDiaryWriteUseCase: DiaryWriteUseCase {
+    
+}
---
+++ MODA/Sources/Domain/UseCase/Diaries/Protocol/DiaryWriteUseCase.swift
@@ -0,0 +1,11 @@
+//
+//  DiaryWriteUseCase.swift
+//  MODA
+//
+//  Copyright (c) 2023 Minii All rights reserved.
+
+import RxSwift
+
+protocol DiaryWriteUseCase: AnyObject {
+    
+}
