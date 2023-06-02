c9552265421f9ddc6bbc5689a149fcdeed501539
미니 <cow970814@naver.com>
2023-06-02 23:43:49 +0900

    feat : Diary Write ViewModel 추가

---
+++ MODA/Sources/ViewModel/DiaryWriteViewModel.swift
@@ -0,0 +1,23 @@
+//
+//  DiaryWriteViewModel.swift
+//  MODA
+//
+//  Copyright (c) 2023 Minii All rights reserved.
+
+import RxSwift
+
+final class DiaryWriteViewModel: ViewModel {
+    struct Input {
+        
+    }
+    
+    struct Output {
+        
+    }
+    
+    var disposeBag = DisposeBag()
+    
+    func transform(input: Input) -> Output {
+        return Output()
+    }
+}
