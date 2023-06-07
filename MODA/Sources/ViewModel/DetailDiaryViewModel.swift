b0910544cac5e87017418446f9c1ab3542e561ea
미니 <cow970814@naver.com>
2023-06-07 15:19:59 +0900

    feat : Detail ViewModel 타입 생성

---
+++ MODA/Sources/ViewModel/DetailDiaryViewModel.swift
@@ -0,0 +1,23 @@
+//
+//  DetailDiaryViewModel.swift
+//  MODA
+//
+//  Copyright (c) 2023 Minii All rights reserved.
+
+import RxSwift
+
+final class DetailDiaryViewModel: ViewModel {
+    struct Input {
+        
+    }
+    
+    struct Output {
+        
+    }
+    
+    var disposeBag = DisposeBag()
+    
+    func transform(input: Input) -> Output {
+        return Output()
+    }
+}
