a520ef20136cfc0fd731a93b906503f4c8010ab4
미니 <cow970814@naver.com>
2023-06-07 23:27:56 +0900

    feat : Detail Diary Use Case 구성

---
+++ MODA/Sources/Domain/UseCase/Diaries/Protocol/DetailDiaryUseCase.swift
@@ -0,0 +1,22 @@
+//
+//  DetailDiaryUseCase.swift
+//  MODA
+//
+//  Copyright (c) 2023 Minii All rights reserved.
+
+import RxSwift
+import Foundation
+
+protocol DetailDiaryUseCase: AnyObject {
+    var selectedId: String { get }
+    var currentDiary: BehaviorSubject<Diary?> { get set }
+    var diaryDate: BehaviorSubject<Date?> { get set }
+    var diaryContent: BehaviorSubject<String?> { get set }
+    var diaryCondition: BehaviorSubject<Diary.Condition?> { get set }
+    var removeDiary: PublishSubject<Void> { get set }
+
+    
+    func fetchCurrentDiary(_ token: String)
+    func updateCurrentDiary(_ token: String)
+    func deleteCurrentDiary(_ token: String)
+}
