4c5af41df01a328d275b8a938f330dd8d6c8464b
미니 <cow970814@naver.com>
2023-06-10 15:08:13 +0900

    feat : User Service 타입 API 타입 케이스 추가

--- MODA/Sources/Utility/Services/UserService.swift
+++ MODA/Sources/Utility/Services/UserService.swift
@@ -8,6 +8,8 @@ import RxSwift
 
 protocol UserServicing: AnyObject {
     func saveInstallation(with body: [String: Any])
+    
+    func createUser(with body: [String: Any]) -> Observable<User>
 }
 
 final class UserService: UserServicing {
@@ -17,25 +19,28 @@ final class UserService: UserServicing {
             .subscribe()
             .dispose()
     }
+    
+    func createUser(with body: [String: Any]) -> Observable<User> {
+        let api = API.createUser(body: body)
+        return DefaultNetworkService().request(to: api)
+    }
 }
 
 private extension UserService {
     enum API {
         case saveInstallation(body: [String: Any])
+        case createUser(body: [String: Any])
     }
 }
 
 extension UserService.API: APIType {
     var baseURL: String {
-        switch self {
-        case .saveInstallation:
-            return "https://parseapi.back4app.com"
-        }
+        return "https://parseapi.back4app.com"
     }
     
     var method: String {
         switch self {
-        case .saveInstallation:
+        case .saveInstallation, .createUser:
             return "POST"
         }
     }
@@ -44,6 +49,8 @@ extension UserService.API: APIType {
         switch self {
         case .saveInstallation:
             return "/parse/installations"
+        case .createUser:
+            return "/users"
         }
     }
     
@@ -58,6 +65,9 @@ extension UserService.API: APIType {
         switch self {
         case .saveInstallation(let body):
             return body
+            
+        case .createUser(let body):
+            return body
         }
     }
     
@@ -67,6 +77,14 @@ extension UserService.API: APIType {
             return [
                 "X-Parse-Application-Id": "T5Idi2coPjEwJ1e30yj8qfgcwvxYHnKlnz4HpyLz",
                 "X-Parse-REST-API-Key": "8EFZ0dSEauC938nFNQ3MVV3rvIgJzKlDsLhIxf9M",
+                "Content-Type": "application/json"
+            ]
+            
+        case .createUser:
+            return [
+                "X-Parse-Application-Id": "T5Idi2coPjEwJ1e30yj8qfgcwvxYHnKlnz4HpyLz",
+                "X-Parse-REST-API-Key": "8EFZ0dSEauC938nFNQ3MVV3rvIgJzKlDsLhIxf9M",
+                "X-Parse-Revocable-Session": "1",
                 "Content-Type": "application/json"
             ]
         }
